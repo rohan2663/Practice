67a327ecbeb02fd96926fda3046de3a76c1ea531
Rohan Kumar <142005785+rohan2663@users.noreply.github.com>
2025-05-21 13:37:57 +0530

    Create Route.js

---
+++ Route.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import ProductList from './features/products/ProductList';
+import About from './About';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>Product App</h1>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        </nav>
+        <hr />
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
